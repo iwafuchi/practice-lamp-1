6c826e5f528a7b3375d9965279fd827fd324deda
iwafuchi <rta375@gmail.com>
2022-07-18 21:52:50 +0900

    update Dockerfile and src/form

--- src/form/input.php
+++ src/form/input.php
@@ -1,14 +1,20 @@
 <?php
+session_start();
+session_regenerate_id();
 
+if (!isset($_SESSION["csrfToken"])) {
+    $_SESSION["csrfToken"] = bin2hex(random_bytes(32));
+}
+$token = $_SESSION["csrfToken"];
 function Sanitize() {
-    if (!empty($_GET)) {
-        foreach ($_GET as $key => $value) {
-            $_GET[$key] = htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+    if (!empty($_POST)) {
+        foreach ($_POST as $key => $value) {
+            $_POST[$key] = htmlspecialchars($value, ENT_QUOTES, "UTF-8");
         }
     }
 }
 Sanitize();
-echo !empty($_GET["name"]) ? "\$_GET[name]の値は{$_GET['name']}です" : "\$_GET[name]の値はemptyです";
+echo !empty($_POST["name"]) ? "\$_POST[name]の値は{$_POST['name']}です" : "\$_POST[name]の値はemptyです";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,10 +27,14 @@ echo !empty($_GET["name"]) ? "\$_GET[name]の値は{$_GET['name']}です" : "\$_
 </head>
 
 <body>
-    <form method="GET" action="input.php">
+    <form method="POST" action="input.php">
         <label>指名<input type="input" name="name"></label>
+        <input type="hidden" name="csrf" value=<?php $token ?>>
         <button type="submit">送信</button>
     </form>
 </body>
+<?php
+echo $_SESSION["csrfToken"] = $token ? "正常値" : "不正値";
+?>
 
 </html>
